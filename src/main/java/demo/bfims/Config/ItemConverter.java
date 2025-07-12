6758df8c90a2b54d916e9a6395189e686d6dc632
chey <cheyanna.graham@gmail.com>
2025-07-12 12:42:38 -0700

    bound item converter generic

--- src/main/java/demo/bfims/Config/ItemConverter.java
+++ src/main/java/demo/bfims/Config/ItemConverter.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 //map itemdto to item child since item is abstract
 @Component
-public class ItemConverter<S, D> implements Converter<S, D> {
+public class ItemConverter<S extends ItemDto, D extends Item> implements Converter<S, D> {
 
     @Autowired
     private ItemRepo itemRepo;
@@ -20,23 +20,22 @@ public class ItemConverter<S, D> implements Converter<S, D> {
     @Override
     public D convert(MappingContext<S, D> mappingContext) {
 
-        if (ItemDto.class.isAssignableFrom(mappingContext.getSource().getClass())) {
-            ItemDto itemDto = (ItemDto) mappingContext.getSource();
+        mappingContext.getSource();
+        ItemDto itemDto = (ItemDto) mappingContext.getSource();
 
-//          If item already exists, return it
-            Long itemId = itemDto.getItemId();
-            if (itemId != null) {
-                Item foundItem = itemRepo.findById(itemId).orElse(null);
-                if (foundItem != null) {
-                    return (D) foundItem;
-                }
+        // If item already exists, return it
+        Long itemId = itemDto.getItemId();
+        if (itemId != null) {
+            Item foundItem = itemRepo.findById(itemId).orElse(null);
+            if (foundItem != null) {
+                return (D)foundItem;
             }
-
-            ItemType itemType = itemDto.getItemType();
-            if (itemType.equals(ItemType.BOOK)) return (D) new Book();
-            else if (itemType.equals(ItemType.JOURNAL)) return (D) new Journal();
-            else if (itemType.equals(ItemType.LITERARY_PIECE)) return (D) new LiteraryPiece();
         }
+
+        ItemType itemType = itemDto.getItemType();
+        if (itemType.equals(ItemType.BOOK)) return (D) new Book();
+        else if (itemType.equals(ItemType.JOURNAL)) return (D) new Journal();
+        else if (itemType.equals(ItemType.LITERARY_PIECE)) return (D) new LiteraryPiece();
 
         return null;
     }
