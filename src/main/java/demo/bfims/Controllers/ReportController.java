f6599061e3e3b90457acfdcdca2a3a420f11384e
chey <cheyanna.graham@gmail.com>
2025-07-13 17:22:03 -0700

    add report service & controller

--- src/main/java/demo/bfims/Controllers/ReportController.java
+++ src/main/java/demo/bfims/Controllers/ReportController.java
@@ -1,10 +1,51 @@
 package demo.bfims.Controllers;
 
+import demo.bfims.DTOs.InventoryDTOs.ItemDto;
+import demo.bfims.Services.ReportSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ReportController {
 
-//    @GetMapping("report/")
+    private ReportSerivce reportSerivce;
+
+    public ReportController(ReportSerivce reportSerivce) {
+        this.reportSerivce = reportSerivce;
+    }
+
+    //Popular items
+//    item id in order items the most
+    @GetMapping("/popularitems")
+    public List<ItemDto> getPopularItems() {
+        return reportSerivce.getPopularItems();
+    }
+
+    //low inventory items
+    @GetMapping("/lowinventory")
+    public List<ItemDto> getLowInventoryItems() {
+        return reportSerivce.getLowInventoryItems();
+    }
+
+//low selling/renting items
+    @GetMapping("lowsales")
+    public List<ItemDto> getLowSalesItems() {
+        return reportSerivce.getLowSalesItems();
+    }
+
+//Items that make the most money
+    @GetMapping("/profitableitems")
+    public List<ItemDto> getProfitableItems() {
+        return reportSerivce.getProfitableItems();
+    }
+
+    @GetMapping("/recentorders")
+    public List<ItemDto> getRecentOrders() {
+        return reportSerivce.getRecentOrders();
+    }
+
+
 }
--- src/main/java/demo/bfims/Services/ReportSerivce.java
+++ src/main/java/demo/bfims/Services/ReportSerivce.java
@@ -1,7 +1,58 @@
 package demo.bfims.Services;
 
+import demo.bfims.DTOs.InventoryDTOs.ItemDto;
+import demo.bfims.Repo.ItemRepo;
+import demo.bfims.Repo.OrderRepo;
+import demo.bfims.Repo.PublicationRepo;
+import demo.bfims.Repo.TransactionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReportSerivce {
+    @Autowired
+    ItemRepo itemRepo;
+    @Autowired
+    OrderRepo orderRepo;
+    @Autowired
+    PublicationRepo publicationRepo;
+    @Autowired
+    TransactionRepo transactionRepo;
+
+    public List<ItemDto> getPopularItems() {
+        System.out.println("Popular Items");
+        return null;
+    }
+
+    public List<ItemDto> getLowInventoryItems() {
+        List<ItemDto> items = new ArrayList<>();
+        System.out.println("Low Inventory Items");
+        return null;
+    }
+
+    //low selling/renting items
+    public List<ItemDto> getLowSalesItems() {
+        System.out.println("Low Sales Items");
+        return null;
+    }
+
+    //Items that make the most money
+    public List<ItemDto> getProfitableItems() {
+        System.out.println("Profitable Items");
+        return null;
+    }
+
+    public List<ItemDto> getRecentOrders() {
+        System.out.println("Recent Orders");
+        return null;
+    }
+
+
+
+
+
+
 }
