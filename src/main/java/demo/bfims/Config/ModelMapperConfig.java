23ae31e56fe051ba7899eb0ac5a65fc473d959c4
chey <cheyanna.graham@gmail.com>
2025-07-19 15:16:07 -0700

    replace converter with provider

--- src/main/java/demo/bfims/Config/ModelMapperConfig.java
+++ src/main/java/demo/bfims/Config/ModelMapperConfig.java
@@ -3,10 +3,16 @@ package demo.bfims.Config;
 import demo.bfims.DTOs.InventoryDTOs.ItemDto;
 import demo.bfims.DTOs.InventoryDTOs.PublicationItemDto;
 import demo.bfims.DTOs.OrderDTOs.TransactionDto;
-import demo.bfims.Entities.Inventory.Item;
-import demo.bfims.Entities.Inventory.PublicationItem;
+import demo.bfims.Entities.Inventory.*;
+import demo.bfims.Entities.Order.Purchase;
+import demo.bfims.Entities.Order.Rental;
 import demo.bfims.Entities.Order.Transaction;
+import demo.bfims.Enums.ItemType;
+import demo.bfims.Enums.PublicationItemType;
+import demo.bfims.Enums.TransactionType;
+import demo.bfims.Repo.ItemRepo;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.Provider;
 import org.modelmapper.TypeMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,28 +24,81 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
 
     @Autowired
-    ItemConverter<ItemDto,Item> itemConverter;
+    ItemRepo itemRepo;
 
-    @Autowired
-    ItemConverter<PublicationItemDto,PublicationItem> publicationItemConverter;
-
-    @Autowired
-    TransactionConverter transactionConverter;
-
-     @Bean
+    @Bean
     public ModelMapper modelMapper() {
-        // tell model mmapper to use custom converter for items
-        // item is abstract and can't be instantiated so requires
-        // special configuration
         ModelMapper modelMapper = new ModelMapper();
-        TypeMap<ItemDto,Item> itemMap = modelMapper.createTypeMap(ItemDto.class,Item.class);
-        itemMap.setConverter(itemConverter);
 
+        Provider<Item> itemProvider = provisionRequest -> {
+            System.out.println("ItemProvider");
+            System.out.println(provisionRequest.getSource());
+
+            ItemDto itemDto = (ItemDto) provisionRequest.getSource();
+
+            // If item already exists, return it (Orders)
+            Long itemId = itemDto.getItemId();
+            if (itemId != null) {
+                Item foundItem = itemRepo.findById(itemId).orElse(null);
+                if (foundItem != null) {
+                    return foundItem;
+                }
+            }
+
+            if (itemDto.getItemType().equals(ItemType.PUBLICATION_ITEM)) {
+                return (PublicationItem) provisionRequest.getSource();
+            }
+            return null;
+        };
+
+        Provider<PublicationItem> publicationItemProvider = provisionRequest -> {
+            System.out.println("PublicationItemProvider");
+            System.out.println(provisionRequest.getSource());
+
+            PublicationItemDto publicationItemDto = (PublicationItemDto) provisionRequest.getSource();
+            PublicationItemType publicationItemType = publicationItemDto.getPublicationItemType();
+
+            if (publicationItemType.equals(PublicationItemType.BOOK))
+                return (Book) provisionRequest.getSource();
+            else if (publicationItemType.equals(PublicationItemType.JOURNAL))
+                return (Journal) provisionRequest.getSource();
+            else if (publicationItemType.equals(PublicationItemType.LITERARY_PIECE))
+                return (LiteraryPiece) provisionRequest.getSource();
+            return null;
+        };
+
+        Provider<Transaction> transactionProvider = provisionRequest -> {
+            System.out.println("Transaction Provider");
+            System.out.println(provisionRequest.getSource());
+            TransactionDto transDto = (TransactionDto) provisionRequest.getSource();
+            Item item = modelMapper.map(transDto.getItem(), Item.class);
+            System.out.println("\nCONVERTED ITEM " + item);
+
+            if (transDto.getTransactionType().equals(TransactionType.RENTAL)) {
+                Rental rental = new Rental();
+                rental.setTransactionType(TransactionType.RENTAL);
+                rental.setItem(item);
+                System.out.println("HERE IN RENTAL");
+                return (Transaction) rental;
+
+            } else if (transDto.getTransactionType().equals(TransactionType.PURCHASE)) {
+                Purchase purchase = new Purchase();
+                purchase.setTransactionType(TransactionType.PURCHASE);
+                purchase.setItem(item);
+                System.out.println("HERE IN PURCHASE");
+                return (Transaction) purchase;
+            }
+            return null;
+        };
+
+        TypeMap<ItemDto, Item> itemMap = modelMapper.createTypeMap(ItemDto.class, Item.class);
         TypeMap<PublicationItemDto, PublicationItem> publicationItemMap = modelMapper.createTypeMap(PublicationItemDto.class, PublicationItem.class);
-        publicationItemMap.setConverter(publicationItemConverter);
+        TypeMap<TransactionDto, Transaction> transactionTypeMap = modelMapper.createTypeMap(TransactionDto.class, Transaction.class);
 
-        TypeMap<TransactionDto, Transaction> transactionMap = modelMapper.createTypeMap(TransactionDto.class,Transaction.class);
-        transactionMap.setConverter(transactionConverter);
+        itemMap.setProvider(itemProvider);
+        publicationItemMap.setProvider(publicationItemProvider);
+        transactionTypeMap.setProvider(transactionProvider);
+
         return modelMapper;
     }
 }
