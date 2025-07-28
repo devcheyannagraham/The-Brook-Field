5fde3e50b130be45c277d2aa7d022ec9c63d7061
chey <cheyanna.graham@gmail.com>
2025-07-28 10:35:28 -0700

    add publication form

---
+++ frontend/src/app/Forms/publication-form/publication-form.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
+
+@Component({
+  selector: 'publication-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './publication-form.component.html',
+  styleUrl: './publication-form.component.css',
+})
+export class PublicationFormComponent {
+  publicationForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.publicationForm = this.formBuilder.group({
+      //Publciation fields
+      title: [''],
+      isbn: [''],
+      datePublished: [''],
+      genre: [''],
+      publicationType: ['book'],
+
+      //PublicationItem fields
+      quantity: [1],
+      edition: [''],
+      format: [''],
+      purchasePrice: [''],
+      rentalRate: [''],
+
+      //No additional Book Fields
+      //Journal Fields
+      issueDate: [''],
+      issueNumber: [''],
+      issueName: [''],
+      volume: [''],
+
+      //Literary Piece
+      literaryType: [''],
+    });
+  }
+
+  submitPublication() {
+    alert('done');
+  }
+}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PublicationFormComponent } from './Forms/publication-form/publication-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'publication',
+    component: PublicationFormComponent,
+  },
+];
