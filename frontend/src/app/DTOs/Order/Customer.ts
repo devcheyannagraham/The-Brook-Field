f31ef2e1690b9342b6bf5c2bc8a11f036a5e5756
chey <cheyanna.graham@gmail.com>
2025-08-05 14:13:35 -0700

    add order fields to customer frontend

--- frontend/src/app/DTOs/Order/Customer.ts
+++ frontend/src/app/DTOs/Order/Customer.ts
@@ -4,5 +4,20 @@ export class Customer {
   public lastName: String;
   public email: String;
   public phoneNumber: String;
+  public city: String;
+  public state: String;
+  public zip: Number;
+  public country: String;
 
+  constructor(formData:any){
+    console.log("New Customer", formData);
+    this.firstName = formData["firstName"];
+    this.lastName = formData["lastName"];
+    this.email = formData["email"];
+    this.phoneNumber = formData["firstName"];
+    this.city = formData["city"];
+    this.state = formData["state"];
+    this.zip = formData["zip"];
+    this.country = formData["country"];
+  }
 }
