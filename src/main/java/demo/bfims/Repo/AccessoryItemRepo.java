defde50245c118886c8a5bed4d999ec489d68c6c
chey <cheyanna.graham@gmail.com>
2025-08-06 06:50:23 -0700

    add repo method

--- src/main/java/demo/bfims/Repo/AccessoryItemRepo.java
+++ src/main/java/demo/bfims/Repo/AccessoryItemRepo.java
@@ -1,10 +1,12 @@
 package demo.bfims.Repo;
 
 import demo.bfims.Entities.Inventory.Accessory.AccessoryItem;
+import demo.bfims.Enums.AccessoryItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface AccessoryItemRepo extends JpaRepository<AccessoryItem, Long> {
     List<AccessoryItem> findAccessoryItemsByAccessory_AccessoryId(Long accessoryId);
+    AccessoryItem findAccessoryItemByAccessory_AccessoryIdAndAccessoryItemStatus(Long accessoryId, AccessoryItemStatus accessoryItemStatus);
 }
