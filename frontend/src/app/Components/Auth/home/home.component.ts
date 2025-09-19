217568cd096b959e034b3734f086f53a599e3586
Chey G <agra759@wgu.edu>
2025-09-19 08:01:28 -0700

    add content to landing page and user home page

--- frontend/src/app/Components/Auth/home/home.component.ts
+++ frontend/src/app/Components/Auth/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { PopularItemsComponent } from "../../Reports/popular-items/popular-items.component";
+import { RecentOrdersComponent } from "../../Reports/recent-orders/recent-orders.component";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'home',
-  imports: [],
+  imports: [PopularItemsComponent, RecentOrdersComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- frontend/src/app/Components/landing-page/landing-page.component.ts
+++ frontend/src/app/Components/landing-page/landing-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { PopularItemsComponent } from "../Reports/popular-items/popular-items.component";
+import { LoginComponent } from "../Auth/login/login.component";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'landing-page',
-  imports: [],
+  imports: [PopularItemsComponent, LoginComponent, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
