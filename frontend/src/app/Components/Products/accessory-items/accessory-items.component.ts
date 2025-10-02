78ec606023f30561ce2b193ed532a646b33ec5a9
Chey G <agra759@wgu.edu>
2025-10-02 10:22:27 -0700

    add buttons to pub & acc view

--- frontend/src/app/Components/Products/accessory-items/accessory-items.component.ts
+++ frontend/src/app/Components/Products/accessory-items/accessory-items.component.ts
@@ -4,12 +4,14 @@ import { Accessory } from '../../../DTOs/Accessory/Accessory';
 import { AccessoryItem } from '../../../DTOs/Accessory/AccessoryItem';
 import { AccessoryService } from '../../../Services/accessory.service';
 import { headers } from '../../../Helpers/headers';
+import { SVGIconComponent } from "../../svgicon/svgicon.component";
 
 @Component({
   selector: 'accessory-items',
   imports: [
     RouterLink,
-  ],
+    SVGIconComponent
+],
   templateUrl: './accessory-items.component.html',
   styleUrl: './accessory-items.component.css'
 })
