f09cb965583965f4baeb1db87d7397dc764ecd0e
Chey G <agra759@wgu.edu>
2025-10-02 11:33:41 -0700

    add form validation for accessory

--- frontend/src/app/Forms/accessory-form/accessory-form.component.ts
+++ frontend/src/app/Forms/accessory-form/accessory-form.component.ts
@@ -4,6 +4,8 @@ import { AccessoryService } from '../../Services/accessory.service';
 import { Accessory } from '../../DTOs/Accessory/Accessory';
 import { AccessoryType } from '../../Enums/AccessoryType';
 import { Router } from '@angular/router';
+import { Validators } from '@angular/forms';
+import { ToasterService } from '../../Services/toaster.service';
 
 @Component({
   selector: 'accessory-form',
@@ -18,7 +20,7 @@ export class AccessoryFormComponent {
   @Input() accessoryId: number;
 
 
-  constructor(private formBuilder: FormBuilder, public accessoryService: AccessoryService, public router: Router) {
+  constructor(private formBuilder: FormBuilder, public accessoryService: AccessoryService, public router: Router, public toasterService:ToasterService) {
   }
 
   ngOnInit() {
@@ -28,10 +30,10 @@ export class AccessoryFormComponent {
 
   createForm() {
     this.accessoryForm = this.formBuilder.group({
-      accessoryName: [''],
-      accessoryType: [''],
-      price: [''],
-      quantity: [1],
+      accessoryName: ['',[Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
+      accessoryType: ['',[Validators.required]],
+      price: ['',[Validators.required, Validators.min(0.1), Validators.max(99999)]],
+      quantity: [1,[Validators.required, Validators.min(0), Validators.max(99999)]],
       accessoryId: []
     });
   }
@@ -56,9 +58,15 @@ export class AccessoryFormComponent {
 
 
   addAccessory() {
-    let accessory = new Accessory(this.accessoryForm.value);
-    accessory.svgIcon = this.accessory.svgIcon;
-    this.saveAccessory(accessory);
+    if (this.accessoryForm.valid) {
+      let accessory = new Accessory(this.accessoryForm.value);
+      accessory.svgIcon = this.accessory?.svgIcon;
+      this.saveAccessory(accessory);
+    }
+    else {
+      this.toasterService.message.set({class:"error", message:"Please complete the form before submitting"})
+
+    }
   }
 
   saveAccessory(accessory: Accessory) {
