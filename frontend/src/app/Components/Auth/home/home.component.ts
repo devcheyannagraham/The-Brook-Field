baf20e09142f4a2b1db61e28f68d48803f94c224
Chey G <agra759@wgu.edu>
2025-10-14 12:50:48 -0700

    remove popularitems from home component

--- frontend/src/app/Components/Auth/home/home.component.ts
+++ frontend/src/app/Components/Auth/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RecentOrdersComponent } from "../../Reports/recent-orders/recent-orders.component";
 import { RouterLink } from '@angular/router';
-import { PopularItemsComponent } from "../../Reports/popular-items/popular-items.component";
 
 
 @Component({
   selector: 'home',
-  imports: [RecentOrdersComponent, RouterLink, PopularItemsComponent],
+  imports: [RecentOrdersComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
